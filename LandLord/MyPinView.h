bf84c474e99b57747eccb56b9fedc7b0910e754f
Alex <alex.xiat@gmail.com>
2013-04-25 21:24:18 -0400

    add pins

---
+++ LandLord/MyPinView.h
@@ -0,0 +1,13 @@
+//
+//  MyPinView.h
+//  LandLord
+//
+//  Created by Alex Xia on 4/25/13.
+//  Copyright (c) 2013 CellularNetwork. All rights reserved.
+//
+
+#import <MapKit/MapKit.h>
+
+@interface MyPinView : MKAnnotationView
+
+@end
