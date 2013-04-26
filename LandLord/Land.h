c7c658499658e184cd6a579bbea0ca363854eb5e
Chong Zhang <cz2276@columbia.edu>
2013-04-25 22:47:49 -0400

    added gesture

---
+++ LandLord/Land.h
@@ -0,0 +1,16 @@
+//
+//  Land.h
+//  LandLord
+//
+//  Created by Chong Zhang on 4/25/13.
+//  Copyright (c) 2013 CellularNetwork. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+
+@interface Land : NSObject
+@property CLLocationCoordinate2D upleft;
+@property CLLocationCoordinate2D bottomright;
+
+@end
---
+++ LandLord/Land.m
@@ -0,0 +1,15 @@
+//
+//  Land.m
+//  LandLord
+//
+//  Created by Chong Zhang on 4/25/13.
+//  Copyright (c) 2013 CellularNetwork. All rights reserved.
+//
+
+#import "Land.h"
+
+@implementation Land
+@synthesize upleft = _upleft;
+@synthesize bottomright = _bottomright;
+
+@end
