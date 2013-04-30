45b2ad6ba48e1671c2ae6e5e8211cc1c10dfea0a
Chong Zhang <cz2276@columbia.edu>
2013-04-30 12:01:30 -0400

    added landname landmsg atomicly refresh

--- LandLord/viewLandDetailViewController.m
+++ LandLord/viewLandDetailViewController.m
@@ -68,6 +68,7 @@
     } else if ([sender tag] == 2) {
         //post land post
         NSLog(@"%@",_landpost.text);
+		_currland.landmsg = _landpost.text;
         
         NSString *originalString = _landpost.text;
         
@@ -90,7 +91,8 @@
 	} else if([sender tag] == 3){
         //post land name
         NSLog(@"%@", _landname.text);
-        
+        _currland.landname = _landname.text;
+		
         NSString *originalstring = _landname.text;
         
         NSString *newstring = [originalstring stringByReplacingOccurrencesOfString:@" " withString:@"%20"];
